03337550a17b7809d8821565ec08146cd59935d0
viuh <37941072+viuh@users.noreply.github.com>
2018-12-15 00:57:03 +0200

    7.14ish

---
+++ bloglist-frontend/.eslintrc.js
@@ -0,0 +1,41 @@
+
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": "off",
+        "linebreak-style": [
+            "warn",
+            "unix"
+        ],
+        "quotes": [
+            "warn",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "eqeqeq": "error",
+        "no-trailing-spaces": "error",
+        "object-curly-spacing": [
+            "error", "always"
+        ],
+        "arrow-spacing": [
+            "error", { "before": true, "after": true }
+        ],
+        "no-console": 0
+    },
+    "globals": {
+        "test": true,
+        "expect": true,
+        "describe": true
+    },
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaVersion": 2017
+    }
+};
